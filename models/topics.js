58099e97440d07082fb008bbe61ab6e1adead41e
OzzyCF <o5wa1@hotmail.com>
2023-10-02 14:19:27 +0100

    removed console from topics model

--- models/topics.js
+++ models/topics.js
@@ -2,7 +2,6 @@ const db = require("../db/connection");
 
 exports.fetchAllTopics = () => {
   return db.query("SELECT * FROM topics;").then((result) => {
-    console.log("Data from the database:", result.rows);
     return result.rows;
   });
 };
