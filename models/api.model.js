15368b066a4dc51f6bf92ac5adf9d8d24824c88d
OzzyCF <o5wa1@hotmail.com>
2023-10-05 10:10:07 +0100

    implement comment_count complete and minor changes to test suite based on feedback

--- models/api.model.js
+++ models/api.model.js
@@ -4,7 +4,14 @@ const db = require("../db/connection");
 
 exports.fetchArticleById = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id=$1`, [article_id])
+    .query(
+      `SELECT articles.*, COUNT(comments.comment_id)::INTEGER  AS comment_count 
+    FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    WHERE articles.article_id=$1
+    GROUP BY articles.article_id;`,
+      [article_id]
+    )
     .then(({ rows }) => {
       if (!rows.length) {
         return Promise.reject({ status: 404, msg: "Article not found" });
